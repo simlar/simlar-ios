6d269c6162d2e51bd032ed1b15675880b6d9576c
Ben Sartor <mail@ben-sartor.de>
2014-10-27 01:36:22 +0100

    [SMLRAppDelegate] when becoming active only check for incoming calls if voip push notifications are not supported

--- Simlar/src/SMLRAppDelegate.m
+++ Simlar/src/SMLRAppDelegate.m
@@ -95,7 +95,9 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 
     SMLRLogFunc;
-    [self checkForIncomingCalls];
+    if (![SMLRPushNotifications isVoipSupported]) {
+        [self checkForIncomingCalls];
+    }
 
     // Hack to silent ringtone after app was opened
     [UIApplication.sharedApplication setApplicationIconBadgeNumber:1];
