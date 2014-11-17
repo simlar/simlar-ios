a1ffcaac5c6325237234ab46d9b01a6504f346ef
Ben Sartor <ben@simlar.org>
2014-11-17 07:30:44 +0100

    [resize_images.sh] add script to scale images

---
+++ resize_images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## exit if an error occurs or on unset variables
+set -eu -o pipefail
+
+declare -r SRC_DIR="$(dirname $(greadlink -f $0))/Simlar/Images.xcassets/"
+
+function resize()
+{
+	local -r SRC=$1
+	local -r DST=$2
+	local -r SCALE=$3
+
+	rm -f ${DST}
+	convert "${SRC}" -strip -resize "${SCALE}" "${DST}"
+	file "${DST}"
+}
+
+find "${SRC_DIR}" -type f -name "*@3x.png" | sort | while read IMAGE; do
+	IMAGE_NAME=$(dirname "${IMAGE}")/$(basename --suffix=@3x.png "${IMAGE}")
+
+	resize "${IMAGE}" "${IMAGE_NAME}@1x.png" "33.33%"
+	resize "${IMAGE}" "${IMAGE_NAME}@2x.png" "66.67%"
+done
