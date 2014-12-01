66525412fb0683e58051a1841188633551270253
Ben Sartor <mail@ben-sartor.de>
2014-12-01 12:55:53 +0100

    [Settings.bundle] version field is now updated at build time

--- update-version.sh
+++ update-version.sh
@@ -7,3 +7,6 @@ declare -r GIT_RELEASE_VERSION=$(git describe --tags --always --dirty)
 
 defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}" "CFBundleShortVersionString" "${GIT_RELEASE_VERSION}"
 defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}" "CFBundleVersion" "${GIT_RELEASE_VERSION}"
+
+### ATTENTION: this assumes the version is the 4th property
+/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:3:DefaultValue ${GIT_RELEASE_VERSION}" "${BUILT_PRODUCTS_DIR}/Simlar.app/Settings.bundle/Root.plist"
