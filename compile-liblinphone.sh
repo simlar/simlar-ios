6013dbbde2f963d7bec0042bbca34b070bb01fdc
Ben Sartor <ben@simlar.org>
2015-10-15 02:20:05 +0200

    [compile-liblinphone.sh] renamed variable

--- compile-liblinphone.sh
+++ compile-liblinphone.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 declare -r  BUILD_DIR=${1:?"Please give liblinphone dir as first parameter"}
 declare -r  GIT_HASH=${2:-"unknown"}
 
-declare -r DST="$(dirname $(readlink -f $0))"
+declare -r DEST_DIR="$(dirname $(readlink -f $0))"
 
 cd "${BUILD_DIR}/linphone-iphone"
 
@@ -27,6 +27,6 @@ unzip -o $(find "linphone-iphone/" -maxdepth 1 -name liblinphone-iphone-sdk\*.zi
 rm -rf liblinphone-tutorials
 rm -rf liblinphone-sdk/apple-darwin/share/
 
-rm -rf "${DST}/liblinphone-sdk"
-mv liblinphone-sdk "${DST}/"
+rm -rf "${DEST_DIR}/liblinphone-sdk"
+mv liblinphone-sdk "${DEST_DIR}/"
 echo "liblinphone build successfull with git hash: ${GIT_HASH}"
