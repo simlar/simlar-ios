3dfebe92a37e74f728d4d0cfd49f518b1c15fbde
Ben Sartor <ben@simlar.org>
2019-06-21 19:59:03 +0200

    [pods] remove Pods from git

--- Pods/Headers/Public/libPhoneNumber-iOS/NBRegularExpressionCache.h
+++
@@ -1,1 +0,0 @@
-../../../libPhoneNumber-iOS/libPhoneNumber/Internal/NBRegularExpressionCache.h
--- Pods/Target Support Files/Pods-Simlar/Pods-Simlar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Simlar : NSObject
-@end
-@implementation PodsDummy_Pods_Simlar
-@end
