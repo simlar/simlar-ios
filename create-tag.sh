c71ff78c4c55f7e5cd8b70d481277311b62c9a52
Ben Sartor <ben@simlar.org>
2019-07-13 16:25:44 +0200

    [create-tag.sh] support branches

--- create-tag.sh
+++ create-tag.sh
@@ -3,11 +3,12 @@
 ## exit if an error occurs or on unset variables
 set -eu -o pipefail
 
-declare -r USAGE="Usage example: $0 1 0 0"
+declare -r USAGE="Usage example: $0 1 0 0 [master]"
 
 declare -ri VERSION_MAJOR=${1?${USAGE}}
 declare -ri VERSION_MINOR=${2?${USAGE}}
 declare -ri VERSION_BUGFIX=${3?${USAGE}}
+declare -r  BRANCH=${4:-"master"}
 
 declare -r SIMLAR_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUGFIX}"
 echo "creating tag: '${SIMLAR_VERSION}'"
@@ -18,10 +19,10 @@ if ! git diff --quiet ; then
         exit 1
 fi
 
-git checkout master
+git checkout "${BRANCH}"
 git fetch
 git fetch --tags
-git pull --rebase
+git pull --rebase origin "${BRANCH}"
 
 
 git tag -s "${SIMLAR_VERSION}" -m "Version: ${SIMLAR_VERSION}"
