2ea023677eeada5f6695a99346054c52ca1f6b45
Ben Sartor <ben@simlar.org>
2020-12-17 00:39:29 +0100

    [liblinphone] add self compiled build 4.4.17

---
+++ liblinphone/linphone-sdk/apple-darwin/Frameworks/linphone.framework/Headers/call.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2010-2019 Belledonne Communications SARL.
+ *
+ * This file is part of Liblinphone.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LINPHONE_CALL_H
+#define LINPHONE_CALL_H
+
+#include "linphone/api/c-call.h"
+#include "linphone/api/c-call-cbs.h"
+#include "linphone/api/c-call-stats.h"
+
+#endif /* LINPHONE_CALL_H */
